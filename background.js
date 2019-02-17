e2fc8ed609ed2afd270faf487b9a6881fc305e73
leoliu <cubemail88@gmail.com>
2019-02-17 15:14:09 +0800

    add layout for contextMenu

--- background.js
+++ background.js
@@ -3,6 +3,30 @@
 // found in the LICENSE file.
 
 'use strict';
+function createMenus(){
+  var parent = chrome.contextMenus.create({
+    "id": "parent",
+    "title": "Pretend not using Ptt",
+    "contexts": ["all"],
+  })
+
+  var launchChangeCSS = chrome.contextMenus.create({
+    "id" : "launch",
+    "title": "Launch app",
+    "contexts": ['all'],
+    "parentId" : parent
+  })
+
+  var pauseChangeCSS = chrome.contextMenus.create({
+    "id": "Pause",
+    "title" : "Pause app",
+    "contexts": ['all'],
+    "parentId": parent
+  })
+}
+
+createMenus()
+
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
